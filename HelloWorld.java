44b9879157fdf855d9ed7af66973e132ca0afe3a
𝓦𝓲𝓵𝓶𝓮𝓻 𝓞𝓻𝓽𝓮𝔃 <wilmerpegoortez@s-MacBook-Air-2.local>
2026-08-27 14:31:35 -0400

    descriptive
    Added another print statement to the program.

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld
     {
         
                 System.out.println("Hello World!");
+                System.out.println("Program modified, but able to revert to original version using GitHub Desktop.");
     }
 } 
